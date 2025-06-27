0a252b84c24de98ff3cce8c7912eef337e30d119
MRG301 <146374475+MRG301@users.noreply.github.com>
2025-06-27 00:50:42 -0600

    Make WebClient base URL configurable

--- src/main/java/mx/edu/uacm/is/slt/as/sistemapolizas/config/WebClientConfig.java
+++ src/main/java/mx/edu/uacm/is/slt/as/sistemapolizas/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package mx.edu.uacm.is.slt.as.sistemapolizas.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,7 +8,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
     @Bean
-    WebClient polizaWebClient() {
-        return WebClient.builder().baseUrl("http://nachintoch.mx:8080").build();
+    WebClient polizaWebClient(@Value("${remote.base-url}") String baseUrl) {
+        return WebClient.builder().baseUrl(baseUrl).build();
     }
 }
