8ae1a19215ef55eba5bb0107e5188b9bb66add1e
MRG301 <146374475+MRG301@users.noreply.github.com>
2025-06-27 02:11:55 -0600

    Fix WebClientConfig to resolve syntax errors

--- src/main/java/mx/edu/uacm/is/slt/as/sistemapolizas/config/WebClientConfig.java
+++ src/main/java/mx/edu/uacm/is/slt/as/sistemapolizas/config/WebClientConfig.java
@@ -1,10 +1,7 @@
 package mx.edu.uacm.is.slt.as.sistemapolizas.config;
 
 import org.springframework.beans.factory.annotation.Value;
-    @Value("${remote.base-url:http://nachintoch.mx:8080}")
-    private String baseUrl;
-
-    public WebClient polizaWebClient() {
+    public WebClient polizaWebClient(@Value("${remote.base-url:http://nachintoch.mx:8080}") String baseUrl) {
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
