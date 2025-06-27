b228066f8769d3fd3951195ab26c669c0ecd8ee9
MRG301 <146374475+MRG301@users.noreply.github.com>
2025-06-27 01:38:00 -0600

    Fix WebClientConfig bean

--- src/main/java/mx/edu/uacm/is/slt/as/sistemapolizas/config/WebClientConfig.java
+++ src/main/java/mx/edu/uacm/is/slt/as/sistemapolizas/config/WebClientConfig.java
@@ -1,18 +1,15 @@
 package mx.edu.uacm.is.slt.as.sistemapolizas.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
-    @Value("${remote.base-url:http://nachintoch.mx:8080}")
-    private String baseUrl;
-
     @Bean
-    WebClient polizaWebClient() {
+    WebClient polizaWebClient(@Value("${remote.base-url:http://nachintoch.mx:8080}") String baseUrl) {
         return WebClient.builder().baseUrl(baseUrl).build();
     }
 }
