eaba49d7dafcd449b879150a92a45dd8299e3db6
MRG301 <146374475+MRG301@users.noreply.github.com>
2025-06-27 01:51:02 -0600

    Simplify WebClientConfig

--- src/main/java/mx/edu/uacm/is/slt/as/sistemapolizas/config/WebClientConfig.java
+++ src/main/java/mx/edu/uacm/is/slt/as/sistemapolizas/config/WebClientConfig.java
@@ -1,7 +1,10 @@
 package mx.edu.uacm.is.slt.as.sistemapolizas.config;
 
 import org.springframework.beans.factory.annotation.Value;
-    public WebClient polizaWebClient(@Value("${remote.base-url:http://nachintoch.mx:8080}") String baseUrl) {
+    @Value("${remote.base-url:http://nachintoch.mx:8080}")
+    private String baseUrl;
+
+    public WebClient polizaWebClient() {
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
