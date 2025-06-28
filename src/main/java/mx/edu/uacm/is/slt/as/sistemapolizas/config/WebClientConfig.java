dae7d306e3a9e27b65883954069d2e0a2b44751e
MRG301 <146374475+MRG301@users.noreply.github.com>
2025-06-28 00:39:13 -0600

    Add beneficiary synchronization logic and tests

--- src/main/java/mx/edu/uacm/is/slt/as/sistemapolizas/config/WebClientConfig.java
+++ src/main/java/mx/edu/uacm/is/slt/as/sistemapolizas/config/WebClientConfig.java
@@ -2,6 +2,7 @@ package mx.edu.uacm.is.slt.as.sistemapolizas.config;
 
 import org.springframework.beans.factory.annotation.Value;
     public WebClient polizaWebClient(@Value("${remote.base-url:http://nachintoch.mx:8080}") String baseUrl) {
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
